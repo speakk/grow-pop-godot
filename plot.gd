e82e6f2409b8c11363c7c0d30d76d21588d74d4b
speak <speakk@gmail.com>
2025-11-06 14:06:58 +0200

    Show hover shape

--- plot.gd
+++ plot.gd
@@ -1,1 +1,11 @@
 class_name Plot extends Node3D
+
+@export var hover_mesh: MeshInstance3D
+
+
+func _on_area_3d_mouse_entered() -> void:
+	hover_mesh.show()
+
+
+func _on_area_3d_mouse_exited() -> void:
+	hover_mesh.hide()
